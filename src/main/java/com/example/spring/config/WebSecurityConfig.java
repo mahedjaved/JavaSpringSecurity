7ae14f421b6a9852c656a77eb36910d66cdf500a
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-01 19:06:42 +0100

    replaces old auth with new and adds users via data.sql

---
+++ src/main/java/com/example/spring/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.spring.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public UserDetailsService users(DataSource dataSource) {
+        // UserDetails user = User.withDefaultPasswordEncoder()
+        // .username("user")
+        // .password("password")
+        // .roles("USER")
+        // .build();
+
+        // return new InMemoryUserDetailsManager(user);
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public static PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
