091da0a478e15bc7be83c9859e9a4093a9bd3478
mahedjaved <mahed95@gmail.com>
2024-06-02 20:18:22 +0100

    adds BrcryptHashEncoder to SecurityConfig and removes users and authorities table from schema.sql and data.sql

--- src/main/java/com/example/spring/config/WebSecurityConfig.java
+++ src/main/java/com/example/spring/config/WebSecurityConfig.java
@@ -1,17 +1,19 @@
 package com.example.spring.config;
 
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,26 +23,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
+                .antMatchers("/customers/**").hasRole("USER")
+                .antMatchers("/orders").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error")
+                .permitAll()
+                .and()
+                .logout()
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+        ;
     }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0}, ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=landon,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+
+    }
     @Bean
     public UserDetailsService users(DataSource dataSource) {
-        // UserDetails user = User.withDefaultPasswordEncoder()
-        // .username("user")
-        // .password("password")
-        // .roles("USER")
-        // .build();
-
-        // return new InMemoryUserDetailsManager(user);
         return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
-    public static PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public GrantedAuthoritiesMapper authoritiesMapper() {
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        return authorityMapper;
     }
-
 }
